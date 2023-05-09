7163c09b4ac8a797c571b0a6483a7bc831444cda
shunjikonishi <swandouble@gmail.com>
2023-05-09 22:59:11 +0900

    println again

--- backend/internal/external/mysql.go
+++ backend/internal/external/mysql.go
@@ -22,6 +22,7 @@ func SetupDB() {
 		username += ":" + config.DBPassword
 	}
 	dsn := fmt.Sprintf("%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, host, port, dbname)
+	fmt.Println("Password: " + config.DBPassword)
 	fmt.Println(dsn)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
