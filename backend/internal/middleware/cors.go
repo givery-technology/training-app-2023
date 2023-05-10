174eb2d57591ab8406ec89af8d4ed635231849a3
shunjikonishi <swandouble@gmail.com>
2023-05-10 14:52:21 +0900

    Add println

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -1,12 +1,15 @@
 package middleware
 
 import (
+	"fmt"
+	myConfig "myapp/internal/config"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	myConfig "myapp/internal/config"
 )
 
 func Cors() gin.HandlerFunc {
+	fmt.Println("CorsAllowOrigin: ", myConfig.CorsAllowOrigin)
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{myConfig.CorsAllowOrigin}
 	config.AllowCredentials = true
